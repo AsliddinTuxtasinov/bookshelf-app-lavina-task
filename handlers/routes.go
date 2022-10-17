c407b8afc06c9aa21f4ddfa04f466637bc3946ea
Asliddin <asliddintukhtasinov5@gmail.com>
2022-10-17 20:39:34 +0500

    update handler file

--- handlers/routes.go
+++ handlers/routes.go
@@ -3,12 +3,18 @@ package handlers
 import (
 	"bookshelf-app/middleware"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
 	router := gin.New()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data":    "asliddin",
+		})
+	})
 	router.POST("/books", middleware.Auth, CreateBook)
 	router.GET("/books", middleware.Auth, GetAllBooks)
 	router.PATCH("/books/:book_id", middleware.Auth, UpdateBook)
