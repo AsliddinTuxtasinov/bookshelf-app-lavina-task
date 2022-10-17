a967a8f535cf88d921889276e28f0681638842bb
Asliddin <asliddintukhtasinov5@gmail.com>
2022-10-17 22:48:44 +0500

    update

--- models/userModel.go
+++ models/userModel.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name   string `json:"name"`
-	Key    string `json:"key" gorm:"unique"`
+	Key    string `json:"key"`
 	Secret string `json:"secret"`
 }
